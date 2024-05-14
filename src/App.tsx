a52361e60c0c2e71f1b97b597e5ce35f9bd781e2
mary-ue <maryandreeva100@gmail.com>
2024-05-15 00:39:21 +0300

    start project, add input and button

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import { useState, MouseEvent } from 'react';
+import Button from './components/Button/Button';
+import Input from './components/Input/Input';
+
+function App() {
+  const [counter, setCounter] = useState<number>(0);
+
+  const addCounter = (evt: MouseEvent) => {
+    console.log(evt);
+  };
+
+  return (
+    <>
+      <Button onClick={addCounter}>Кнопка</Button>
+      <Button appearence='big' onClick={addCounter}>Кнопка</Button>
+      <Input placeholder='Email' />
+    </>
+  );
+}
+
+export default App;
