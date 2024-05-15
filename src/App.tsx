c0846a5d2c36afae8476e7ce5ca745009d80aa04
mary-ue <maryandreeva100@gmail.com>
2024-05-15 21:33:39 +0300

    add sidebar

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { useState, MouseEvent } from 'react';
 import Button from './components/Button/Button';
 import Input from './components/Input/Input';
+import { Link } from 'react-router-dom';
 
 function App() {
   const [counter, setCounter] = useState<number>(0);
