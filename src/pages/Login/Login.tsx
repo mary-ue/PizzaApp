bc0b6a7f477d797852bee1babcfcfb2ebc136701
mary-ue <maryandreeva100@gmail.com>
2024-05-18 17:01:34 +0300

    change auth to redux

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -3,10 +3,10 @@ import Button from '../../components/Button/Button';
 import { Headling } from '../../components/Headling/Headling';
 import Input from '../../components/Input/Input';
 import styles from './Login.module.css';
-import { FormEvent, useState } from 'react';
-import axios, { AxiosError } from 'axios';
-import { PREFIX } from '../../helpers/API';
-import { LoginResponse } from '../../interfaces/auth.interface';
+import { FormEvent, useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { AppDispatch, RootState } from '../../store/store';
+import { login, userActions } from '../../store/user.slice';
 
 export type LoginForm = {
   email: {
@@ -18,13 +18,13 @@ export type LoginForm = {
 }
 
 export const Login = () => {
-  const [error, setError] = useState<string | null>();
   const navigate = useNavigate();
+  const dispatch = useDispatch<AppDispatch>();
+  const { jwt, loginErrorMessage } = useSelector((s: RootState) => s.user);
 
   const submit = async (e: FormEvent) => {
     e.preventDefault();
-    setError(null);
-    // console.log(e);
+    dispatch(userActions.clearLoginError());
     
     const target = e.target as typeof e.target & LoginForm;
     const { email, password } = target;
@@ -33,26 +33,19 @@ export const Login = () => {
   };
 
   const sendLogin = async (email: string, password: string) => {
-    try {
-      const { data } = await axios.post<LoginResponse>(`${PREFIX}/auth/login`, {
-        email, 
-        password
-      });
-      console.log(data);
-      localStorage.setItem('jwt', data.access_token);
-      navigate('/');
-    } catch (error) {
-      if (error instanceof AxiosError) {
-        console.error(error);
-        setError(error.response?.data.message);
-      }
-    }
+    dispatch(login({ email, password }));
   };
+
+  useEffect(() => {
+    if (jwt) {
+      navigate('/');
+    }
+  }, [jwt, navigate]);
 
   return (
     <div className={styles.login}>
       <Headling>Вход</Headling>
-      {error && <div className={styles.error}>{error}</div>}
+      {loginErrorMessage && <div className={styles.error}>{loginErrorMessage}</div>}
       <form className={styles.form} onSubmit={submit}>
         <div className={styles.field}>
           <label htmlFor="email">Ваш email</label>
---
+++ src/store/user.slice.ts
@@ -0,0 +1,63 @@
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+import { loadState } from './storage';
+import { LoginResponse } from '../interfaces/auth.interface';
+import axios, { AxiosError } from 'axios';
+import { PREFIX } from '../helpers/API';
+
+export const JWT_PERSISTENT_STATE = 'userData';
+
+export interface UserPersistentState {
+	jwt: string | null;
+}
+
+export interface UserState {
+	jwt: string | null;
+  loginErrorMessage?: string;
+}
+
+const initialState: UserState = {
+	jwt: loadState<UserPersistentState>(JWT_PERSISTENT_STATE)?.jwt ?? null
+};
+
+export const login = createAsyncThunk('user/login',
+	async (params: { email: string, password: string }) => {
+    try {
+      const { data } = await axios.post<LoginResponse>(`${PREFIX}/auth/login`, {
+        email: params.email,
+        password: params.password
+      });
+      return data;
+    } catch (error) {
+      if (error instanceof AxiosError) {
+        throw new Error(error.response?.data.message);
+      }
+    }
+	}
+);
+
+export const userSlice = createSlice({
+	name: 'user',
+	initialState,
+	reducers: {
+		logout: (state) => {
+			state.jwt = null;
+		},
+    clearLoginError: (state) => {
+      state.loginErrorMessage = undefined;
+    }
+	},
+	extraReducers: (builder) => {
+		builder.addCase(login.fulfilled, (state, action) => {
+			if (!action.payload) { 
+        return;
+      }
+      state.jwt = action.payload.access_token;
+		});
+    builder.addCase(login.rejected, (state, action) => {
+			state.loginErrorMessage = action.error.message;
+		});
+	}
+});
+
+export default userSlice.reducer;
+export const userActions = userSlice.actions;
