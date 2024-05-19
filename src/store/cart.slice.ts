ff0b93dbd396630f39632e90e3663586da2ef84f
mary-ue <maryandreeva100@gmail.com>
2024-05-20 00:39:00 +0300

    cart

--- src/store/cart.slice.ts
+++ src/store/cart.slice.ts
@@ -1,22 +1,45 @@
 import { PayloadAction, createSlice } from '@reduxjs/toolkit';
+import { loadState } from './storage';
+
+export const CART_PERSISTENT_STATE = 'cartData';
 
 export interface CartItem {
   id: number;
   count: number;
 }
 
-export interface CartSlice {
-  items: CartItem[];
+export interface CartState {
+	items: CartItem[];
 }
 
-const initialState: CartSlice = {
-  items: []
+const initialState: CartState = loadState<CartState>(CART_PERSISTENT_STATE) ?? {
+	items: []
 };
 
 export const cartSlice = createSlice({
   name: 'cart',
   initialState,
   reducers : {
+    delete: (state, action: PayloadAction<number>) => {
+      state.items = state.items.filter(i => i.id !== action.payload);
+    },
+    remove: (state, action: PayloadAction<number>) => {
+      const existed = state.items.find(i => i.id === action.payload);
+      if (!existed) {
+        return;
+      }
+      if (existed.count === 1) {
+        state.items = state.items.filter(i => i.id !== action.payload);
+      } else {
+        state.items.map(i => {
+          if (i.id === action.payload) {
+            i.count -= 1;
+          }
+          return i;
+        });
+        return;
+      }
+    },
     add: (state, action: PayloadAction<number>) => {
       const existed = state.items.find(i => i.id === action.payload);
       if (!existed) {
